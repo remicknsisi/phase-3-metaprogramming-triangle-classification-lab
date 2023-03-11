0900c41744e10d7da54a8c361290a7c67c5e5f47
Sienna <remicknsisi@gmail.com>
2023-03-11 09:32:51 -0500

    commit tree 81e75cc87d6c1599b662134d851289fa1fa31b35

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  def initialize l1, l2, l3
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    if @l1 <= 0 || @l2 <= 0 || @l3 <= 0
+      raise TriangleError
+    elsif (@l1 + @l2 <= @l3) || (@l1 + @l3 <= @l2) || (@l3 + @l2 <= @l1)
+      raise TriangleError
+    elsif @l1 == @l2 && @l1 == @l3
+      :equilateral
+    elsif @l1 == @l2 || @l1 == @l3 || @l2 == @l3
+      :isosceles
+    elsif @l1 != @l2 && @l1 != @l3 && @l2 != @l3
+      :scalene
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    'Not a valid triangle!'
+  end
 end
